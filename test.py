686751ca0c1b65dd9e61d70300e5e27d86b5d5da
SmallTurtlee <na.sem.2007@yandex.ru>
2024-04-02 11:36:00 +0300

    График по таблице

--- test.py
+++ test.py
@@ -1,38 +1,74 @@
-from PyQt5.QtWidgets import     QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton
+from PyQt5.QtWidgets import     QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton, QHBoxLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 import sys
 import openpyxl
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+from PyQt5 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 class Main(QWidget):
     def __init__(self):
         super(Main, self).__init__()
         self.setWindowTitle("Loading Excel")
-
         layout = QVBoxLayout()
+        block = QHBoxLayout()
         self.setLayout(layout)
-        self.button = QPushButton("Open new file")
-        self.button.clicked.connect(self.on_click)
+        self.button1 = QPushButton("Open new file")
+        self.button2 = QPushButton("Create plotting")
+        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+        self.button1.clicked.connect(self.on_click)
+        self.button2.clicked.connect(self.on_click2)
         self.table_widget = QTableWidget()
-        layout.addWidget(self.button)
-        layout.addWidget(self.table_widget)
+        block.addWidget(self.button1)
+        block.addWidget(self.button2)
+        layout.addLayout(block)
+        layout.addWidget(self.sc)
+        layout.addWidget(self.table_widget)   
+
 
         
     @pyqtSlot()
     def on_click(self):
         name, done1 = QtWidgets.QInputDialog.getText(
 			self, 'Input Dialog', 'Введите название или путь к нужному файлу:') 
-        if done1: self.load_data(name)
+        if done1: 
+            self.load_data(name)
+            
+
+
+    def on_click2(self):
+        self.sc.axes.clear()
+        list_values = list(self.sheet.values)
+        legend = list(list_values[0][1:])
+        Main_sheet = list(reversed(list(zip(*list_values[1:]))))
+        for i in range(len(legend)):
+            self.sc.axes.plot(Main_sheet[-1], Main_sheet[-1*(2+i)])
+        self.sc.draw()
+        
 
     def load_data(self, path):
+        workbook = None
         workbook = openpyxl.load_workbook(path)
-        sheet = workbook.active
+        self.sheet = workbook.active
 
-        self.table_widget.setRowCount(sheet.max_row)
-        self.table_widget.setColumnCount(sheet.max_column)
+        self.table_widget.setRowCount(self.sheet.max_row - 1)
+        self.table_widget.setColumnCount(self.sheet.max_column)
 
-        list_values = list(sheet.values)
+        list_values = list(self.sheet.values)
         self.table_widget.setHorizontalHeaderLabels(list_values[0])
 
         row_index = 0
