4efa972cb5d3f3dbef3f87ee7a18aa3fa1402ebe
SmallTurtlee <na.sem.2007@yandex.ru>
2024-04-09 14:58:27 +0300

    15 дня 9 апреля

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ class Main(QWidget):
     def __init__(self):
         super(Main, self).__init__()
         self.setWindowTitle("Loading Excel")
-        self.w1=[]
+        self.checkboxes=[]
         self.number_of_grafics = 1
         self.canvases = []
         layout = QVBoxLayout() 
@@ -29,7 +29,10 @@ class Main(QWidget):
         checkboxes1 = QGroupBox()
         checkboxes1.setTitle("График №1")
         checkboxes1.setFixedWidth(250)
-        self.boxes1 = QVBoxLayout()
+        self.boxes = []
+        self.boxes.append(QVBoxLayout())
+        self.checkboxes = []
+        self.checkboxes.append([])
         self.setLayout(layout)
         self.canvases.append(MplCanvas(self, width=5, height=4, dpi=100))
         toolbar = NavigationToolbar(self.canvases[0], self)
@@ -38,7 +41,7 @@ class Main(QWidget):
         layout.addWidget(toolbar)               #
         grafic.addWidget(self.canvases[0])
         grafic.addWidget(checkboxes1)
-        checkboxes1.setLayout(self.boxes1)
+        checkboxes1.setLayout(self.boxes[0])
         layout.addLayout(self.manygrafics)      #
         self.manygrafics.addLayout(grafic)
         layout.addWidget(self.table_widget)     #
@@ -69,23 +72,34 @@ class Main(QWidget):
             self.clear_canvases()
             
     def on_click2(self):
-        self.canvases[self.number_of_grafics-1].axes.clear()
+        for i in self.canvases: i.axes.clear()
         list_values = list(self.sheet.values)
         legend = list(list_values[0][1:])
         Main_sheet = list(reversed(list(zip(*list_values[1:]))))
-        for i in range(len(legend)):
-            if self.w1[i].isChecked():
-                self.canvases[self.number_of_grafics-1].axes.plot(Main_sheet[-1], Main_sheet[-1*(2+i)])
-        self.canvases[self.number_of_grafics-1].draw()  
+        for graph in range(self.number_of_grafics):
+            for i in range(len(legend)):
+                if self.checkboxes[graph][i].isChecked():
+                    self.canvases[graph].axes.plot(Main_sheet[-1], Main_sheet[-1*(2+i)])
+            self.canvases[graph].draw()  
 
     def on_click3(self):
         self.number_of_grafics += 1
         grafic = QHBoxLayout()
+        canv = QVBoxLayout()
         checkboxes1 = QGroupBox()
         checkboxes1.setTitle("График №"+str(self.number_of_grafics))
         checkboxes1.setFixedWidth(250)
+        self.boxes.append(QVBoxLayout())
+        self.checkboxes.append([])
+        for i in range(len(self.legend) - 1):
+                self.checkboxes[-1].append(QCheckBox(self.legend[i+1], self))
+                self.boxes[-1].addWidget(self.checkboxes[-1][i])
+        checkboxes1.setLayout(self.boxes[-1])
         self.canvases.append(MplCanvas(self, width=5, height=4, dpi=100))
-        grafic.addWidget(self.canvases[self.number_of_grafics-1])
+        toolbar = NavigationToolbar(self.canvases[self.number_of_grafics-1], self)
+        canv.addWidget(toolbar)
+        canv.addWidget(self.canvases[self.number_of_grafics-1])
+        grafic.addLayout(canv)
         grafic.addWidget(checkboxes1)
         self.manygrafics.addLayout(grafic)
 
@@ -102,7 +116,8 @@ class Main(QWidget):
         self.table_widget.setColumnCount(self.sheet.max_column)
 
         list_values = list(self.sheet.values)
-        self.table_widget.setHorizontalHeaderLabels(list_values[0])
+        self.legend = list(list_values[0])                                   #self.legeng - матрица со всеми названиями, включая ось х
+        self.table_widget.setHorizontalHeaderLabels(self.legend)
 
         row_index = 0
         for value_tuple in list_values[1:]:
@@ -113,13 +128,14 @@ class Main(QWidget):
             row_index += 1
 
     def make_checkboxes(self):
-        list_values = list(self.sheet.values)
-        legend = list(list_values[0][1:])
-        for i in self.w1: self.boxes1.removeWidget(i)
-        self.w1 =[]
-        for i in range(len(legend)):
-            self.w1.append(QCheckBox(legend[i], self))
-            self.boxes1.addWidget(self.w1[i])
+        for graph in range(self.number_of_grafics):
+            for i in self.checkboxes[graph]: self.boxes[graph].removeWidget(i)                
+        self.checkboxes =[]
+        for graph in range(self.number_of_grafics):
+            self.checkboxes.append([])
+            for i in range(len(self.legend) - 1):
+                self.checkboxes[graph].append(QCheckBox(self.legend[i+1], self))
+                self.boxes[graph].addWidget(self.checkboxes[graph][i])
 
 
 app = QApplication(sys.argv)
